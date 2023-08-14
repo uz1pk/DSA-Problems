5e04bbd8cec039c2f85bc976cd0420a25b2b5546
Usman Zaheer <usmanozaheer@gmail.com>
2023-08-13 20:22:45 -0400

    nit

--- same-tree/same-tree.py
+++ same-tree/same-tree.py
@@ -9,7 +9,7 @@ def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
     """
     if not p and not q:
         return True
-    elif not p or not q:
+    if not p or not q:
         return False
     return (
         p.val == q.val and isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
